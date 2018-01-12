f5915736b7cf5b50d215ed35e653e4c18b9ab452
azranmo <moranazran11@gmail.com>
2018-01-12 16:09:49 +0200

    add web angular project

---
+++ AppClient/Services.js
@@ -0,0 +1,72 @@
+﻿app.factory('Event', ['$http', function ($http) {
+    function Event(event) {
+        if (event)
+            this.setData(event);
+    }
+    Event.prototype = {
+        setData: function (eventData) {
+            angular.extend(this, eventData);
+        }
+    };
+    return Garment;
+}]);
+//-------------------------------------------------------------------------------------------------------------------
+app.factory('UserService', ['$http', function ($http) {
+    var service = {};
+    service.isLoggedIn = false;
+    service.success = false;
+    service.login = function (user) {
+        var requestUrl = "http://localhost:54273/Service1.svc/Login";
+        return $http.post(requestUrl, user)
+            .then(function (response) {
+                var userID = response.data;
+                $http.defaults.headers.common = {
+                    'userID': userID,
+                    'user': user.Email
+                };
+                service.isLoggedIn = true;
+
+                return Promise.resolve(response);
+            })
+            .catch(function (e) {
+                return Promise.reject(e);
+            });
+    };
+    service.Register = function (user) {
+        var requestUrl = "http://localhost:54273/Service1.svc/Register";
+        return $http.post(requestUrl, user)
+            .then(function (response) {
+                var userID = response.data;
+                $http.defaults.headers.common = {
+                    'userID': userID,
+                    'user': user.Email
+                };
+                return Promise.resolve(response);
+            })
+            .catch(function (e) {
+                return Promise.reject(e);
+            });
+    };
+    return service;
+}]);
+//-------------------------------------------------------------------------------------------------------------------
+app.service("EventService", function ($http) {
+
+    this.GetEvents = function () {
+        return $http.get("http://localhost:54273/Service1.svc/Events");
+    };
+    this.GetEvent = function (eventID) {
+        return $http.get("http://localhost:54273/Service1.svc/Events/" + eventID);
+    };
+    this.JoinEvent = function (eventID, userID) {
+        return $http.get("http://localhost:54273/Service1.svc/Events/" + eventID + "/" + userID);
+    };
+    this.AddEvent = function (EventDetails) {
+        var request = $http({
+            method: "post",
+            url: "http://localhost:54273/Service1.svc/Events",
+            data: EventDetails
+        });
+        return request;
+    }
+}); 
---
+++ AppClient/app.js
@@ -0,0 +1,186 @@
+﻿var app = angular.module('myApp', ['ngRoute','LocalStorageModule']);
+
+app.config(function (localStorageServiceProvider) {
+    localStorageServiceProvider.setPrefix('angular_App');
+});
+//-------------------------------------------------------------------------------------------------------------------
+app.controller('mainController', ['UserService','$http','$location','localStorageService',function (UserService,$http,$location,localStorageService) {
+    var self = this;
+    self.text2="";
+    self.header2 = "";
+    self.userService = UserService;
+    self.logOut = function () {
+        self.userService.isLoggedIn = false;
+        self.deleteCookie('Password');
+        self.deleteCookie('Email');
+        self.deleteCookie('last entry date');
+        $location.path('/');
+    };
+
+    self.deleteCookie = function (parameter) {
+        var cookieVal = localStorageService.cookie.get(parameter);
+        if (cookieVal) {
+            localStorageService.cookie.remove(parameter);
+        }
+    };
+
+    if( self.userService.isLoggedIn){
+        self.Email = $http.defaults.headers.common.user;
+        if( localStorageService.cookie.get('last entry date')!= null){
+            self.lastDate =  "last entry date: "+ localStorageService.cookie.get('last entry date');
+        }
+    }
+        else
+    {
+        self.Email = 'Guest';
+    }
+
+}]);
+//-------------------------------------------------------------------------------------------------------------------
+app.controller('eventsController', ['$http', 'Event', 'localStorageService','EventService', function ($http, Event, localStorageService, EventService) {
+    var self = this;
+    self.getEvents = function () {
+        EventService.GetEvents().then(function (res) {
+            self.Events = [];
+            angular.forEach(res.data, function (event) {
+                self.Events.push(new Event(event));
+            });
+        }).catch(function (err) {
+            alert(err);
+        });
+    };
+}]);
+//-------------------------------------------------------------------------------------------------------------------
+app.controller('loginController', ['UserService','localStorageService', '$location', '$window','$http',
+    function(UserService,localStorageService, $location, $window, $http) {
+        var self = this;
+        self.user = { Email: '', Password: ''};
+        self.login = function(valid) {
+            if (valid) {
+
+                UserService.login(self.user).then(function (success) {
+                    if ((localStorageService.cookie.get('Email') != null && localStorageService.cookie.get('Password') != null) || (localStorageService.cookie.get('Email')!=self.user.username &&localStorageService.cookie.get('Password')!=self.user.password)){
+                        self.deleteCookie('Email');
+                        self.deleteCookie('Password');
+                        self.deleteCookie('last entry date');
+                        self.cookieKey ='Esername';
+                        self.cookieValue = self.user.Email;
+                        self.addCookie();
+                        self.cookieKey ='Password';
+                        self.cookieValue = self.user.Password;
+                        self.addCookie();
+                        self.cookieKey ='last entry date';
+                        var d = new Date();
+                        var today = d.toDateString();
+                        self.cookieValue = today;
+                        self.addCookie();
+                    }
+                    $location.path('/');
+                    self.getCart();
+
+                }, function (error) {
+                    self.errorMessage = error.data;
+                    $window.alert('Email not found in the system');
+                });
+            }
+        };
+        self.deleteCookie = function (parameter) {
+            var cookieVal = localStorageService.cookie.get(parameter);
+            if (cookieVal) {
+                localStorageService.cookie.remove(parameter);
+            }
+        };
+
+        self.addCookie = function () {
+            var cookieVal = localStorageService.cookie.get(self.cookieKey);
+            if (!cookieVal)
+                localStorageService.cookie.set(self.cookieKey,self.cookieValue, 3);
+        };
+    }]);
+//-------------------------------------------------------------------------------------------------------------------
+app.controller('homeController', ['UserService', '$http', 'Garment','localStorageService','$location', function(UserService, $http, Garment, localStorageService, $location) {
+    var self = this;
+    
+    self.login = function() {
+        self.user = {
+            Email: localStorageService.cookie.get('Email'),
+            Password: localStorageService.cookie.get('Password')
+        };
+        UserService.login(self.user).then(function () {
+            $location.path('/');
+        }, function (error) {
+            self.errorMessage = error.data;
+            $window.alert('Email not found in the system');
+        });
+    };
+}]);
+//-------------------------------------------------------------------------------------------------------------------
+app.controller('registerController',['UserService', '$location', '$window','localStorageService','$scope',
+    function(UserService, $location, $window,localStorageService, $scope) {
+
+        var self = this;
+        self.user = {
+            FirstName: '',
+            LastName: '',
+            PhoneNumber: '',
+            Email: '',
+            Password: ''
+        };
+        self.register = function(valid) {
+            if (valid) {
+                UserService.Register(self.user).then(function (success) {
+                    if(success!=false){
+                        $location.path('/login');
+                        self.cookieKey ='Email';
+                        self.cookieValue = self.user.Email;
+                        self.addCookie();
+                        self.cookieKey ='Password';
+                        self.cookieValue = self.user.Password;
+                        self.addCookie();
+                        self.cookieKey ='last entry date';
+                        var d = new Date();
+                        var today = d.toDateString();
+                        self.cookieValue = today;
+                        self.addCookie();
+                    }
+                    else {
+                        $window.alert('Email already in system');
+                    }
+                }, function (error) {
+                    self.errorMessage = error.data;
+                    $window.alert('Email already in system');
+                })
+            }
+        };
+        self.addCookie = function () {
+            var cookieVal = localStorageService.cookie.get(self.cookieKey);
+            if (!cookieVal)
+                localStorageService.cookie.set(self.cookieKey,self.cookieValue, 3);
+        };
+    }]);
+//-------------------------------------------------------------------------------------------------------------------
+app.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('');
+}]);
+app.config( ['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "./Components/Home/Home.html",
+            controller : "mainController"
+        })
+        .when("/login", {
+            templateUrl : "./Components/Login/Login.html",
+            controller : "loginController"
+        })
+        .when("/events", {
+            templateUrl : "./Components/Events/Events.html",
+            controller : "EventsController"
+        })
+        .when("/register", {
+            templateUrl : "./Components/Register/Register.html",
+            controller : "registerController"
+        })
+        .otherwise({redirect: '/'
+        });
+}]);
+//-------------------------------------------------------------------------------------------------------------------
